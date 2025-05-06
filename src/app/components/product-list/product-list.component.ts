2b0322fd10891affb4ef94747a1341bb5988f7a9
LeonardoKessy <leokessy2005@gmail.com>
2025-05-06 18:57:26 -0300

    Product interface

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from '../../interfaces/product';
 
 @Component({
   selector: 'app-product-list',
@@ -7,16 +8,20 @@ import { Component } from '@angular/core';
   styleUrl: './product-list.component.scss'
 })
 export class ProductListComponent {
-  products = [
+  products: Product[] = [
     {
-      name: "Post-it pack",
-      price: "$2000",
-      image: "assets/img/products/postIts.jpg"
+      name: 'Post-it pack',
+      price: 2000,
+      image: 'assets/img/products/postIts.jpg',
+      type: 'Stationary',
+      stock: 200,
     },
     {
-      name: "Stapler",
-      price: "$10000",
-      image: "assets/img/products/stapler.jpg"
+      name: 'Stapler',
+      price: 10000,
+      image: 'assets/img/products/stapler.jpg',
+      type: 'Stationary',
+      stock: 30,
     }
   ]
 }
